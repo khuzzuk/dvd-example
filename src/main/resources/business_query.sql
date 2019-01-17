599d224425e3e5fff8036e59befe9bd5109b2da5
Adrian Drabik <adriandrabik@gmail.com>
2019-01-17 18:44:46 +0100

    add business query

--- src/main/resources/business_query.sql
+++ src/main/resources/business_query.sql
@@ -1,4 +1,4 @@
---money per film
+--revenue per film
 SELECT f.film_id, sum(p.amount)
 FROM film f
        JOIN inventory i ON f.film_id = i.film_id
@@ -7,7 +7,7 @@ FROM film f
 GROUP BY f.film_id
 ORDER BY f.title;
 
---money per film per day
+--revenue per film per day
 SELECT f.film_id, p.payment_date, SUM(p.amount)
 FROM film f
   JOIN inventory i ON f.film_id = i.film_id
@@ -16,8 +16,24 @@ FROM film f
 GROUP BY f.film_id, p.payment_date
 ORDER BY f.title, p.payment_date;
 
+--revenue per film per day per store
+SELECT f.film_id, i.store_id, p.payment_date, SUM(p.amount)
+FROM film f
+  JOIN inventory i ON f.film_id = i.film_id
+  JOIN rental r ON i.inventory_id = r.inventory_id
+  JOIN payment p ON r.rental_id = p.rental_id
+GROUP BY f.film_id, i.store_id, p.payment_date
+ORDER BY f.title, i.store_id, p.payment_date;
 
-
-
-
-
+--daily cumulative revenue per film per day per store
+SELECT f.film_id, i.store_id, p.payment_date,
+  SUM(SUM(p.amount)) OVER (
+  PARTITION BY f.film_id, i.store_id
+  ORDER BY p.payment_date
+  )
+FROM film f
+  JOIN inventory i ON f.film_id = i.film_id
+  JOIN rental r ON i.inventory_id = r.inventory_id
+  JOIN payment p ON r.rental_id = p.rental_id
+GROUP BY f.film_id, i.store_id, p.payment_date
+ORDER BY f.title, i.store_id, p.payment_date;
