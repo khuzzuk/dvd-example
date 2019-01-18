93969d2bb2808432747fbafa9018f97c27eaf970
Adrian Drabik <adriandrabik@gmail.com>
2019-01-18 11:58:18 +0100

    added hibernate statistics

--- src/test/java/com/example/dvdexample/bussiness/FilmReportingServiceImplSpec.groovy
+++ src/test/java/com/example/dvdexample/bussiness/FilmReportingServiceImplSpec.groovy
@@ -18,51 +18,26 @@ class FilmReportingServiceImplSpec extends Specification {
     }
 
     def "check pure java reporting"() {
-        given:
-        long start = System.nanoTime()
-        println 'used memory: ' + reportMemory() / 1_000_000
-
         when:
         def report = filmReportingService.filmFinancialReport()
-        def t = System.nanoTime() - start
-        println 'worked millis: ' + t / 1_000_000
-        println 'used memory: ' + reportMemory() / 1_000_000
 
         then:
         report.values().size() == 958
     }
 
     def "check jpql query"() {
-        given:
-        long start = System.nanoTime()
-        println 'used memory: ' + reportMemory() / 1_000_000
-
         when:
         def report = filmReportingService.filmFinancialReportSQL()
-        def t = System.nanoTime() - start
-        println 'worked millis: ' + t / 1_000_000
-        println 'used memory: ' + reportMemory() / 1_000_000
 
         then:
         report.size() == 14582
     }
 
     def "check native sql query"() {
-        given:
-        long start = System.nanoTime()
-        println 'used memory: ' + reportMemory() / 1_000_000
-
         when:
         def report = filmReportingService.filmFinancialReportNative()
-        def t = System.nanoTime() - start
-        println 'worked millis: ' + t / 1_000_000
-        println 'used memory: ' + reportMemory() / 1_000_000
 
         then:
         report.size() == 14582
-    }
-
-    static long reportMemory() {
-        Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()
     }
 }
